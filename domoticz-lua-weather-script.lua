70e076988e9ac6134fa028de5cc5845bb29b795b
damsma <45398732+damsma@users.noreply.github.com>
2018-11-28 22:17:56 +0100

    Update domoticz-lua-weather-script.lua

--- domoticz-lua-weather-script.lua
+++ domoticz-lua-weather-script.lua
@@ -15,29 +15,31 @@ end
 
 commandArray = {}
 
--- loop through all the devices
-for deviceName,deviceValue in pairs(otherdevices) do
-    if (deviceName=='Aussen') then
-        --commandArray['UpdateDevice'] = '8888|0|'..deviceValue
-        --print (deviceValue);
+if time.sec == 0 then
+  -- loop through all the devices
+  for deviceName,deviceValue in pairs(otherdevices) do
+      if (deviceName=='Aussen') then
+          --commandArray['UpdateDevice'] = '8888|0|'..deviceValue
+          --print (deviceValue);
 
-        local myTable = deviceValue:split(";")
-        
-        if ((tonumber(myTable[1]) < 10) and (tonumber(myTable[1]) >= 0)) then
-            commandArray['UpdateDevice'] = '8888|0|  '..myTable[1]
-        elseif ((tonumber(myTable[1]) < 0) and (tonumber(myTable[1]) > -10)) then
-            commandArray['UpdateDevice'] = '8888|0| '..myTable[1]
-        elseif (tonumber(myTable[1]) <= -10) then
-            commandArray['UpdateDevice'] = '8888|0|'..myTable[1]
-        else
-            commandArray['UpdateDevice'] = '8888|0| '..myTable[1]
-        end
-        
+          local myTable = deviceValue:split(";")
 
-        --for i = 1, #myTable do
-        --   print( myTable[i] ) -- This will give your needed output
-        --end
-    end
+          if ((tonumber(myTable[1]) < 10) and (tonumber(myTable[1]) >= 0)) then
+              commandArray['UpdateDevice'] = '8888|0|  '..myTable[1]
+          elseif ((tonumber(myTable[1]) < 0) and (tonumber(myTable[1]) > -10)) then
+              commandArray['UpdateDevice'] = '8888|0| '..myTable[1]
+          elseif (tonumber(myTable[1]) <= -10) then
+              commandArray['UpdateDevice'] = '8888|0|'..myTable[1]
+          else
+              commandArray['UpdateDevice'] = '8888|0| '..myTable[1]
+          end
+
+
+          --for i = 1, #myTable do
+          --   print( myTable[i] ) -- This will give your needed output
+          --end
+      end
+  end
 end
 
 return commandArray
